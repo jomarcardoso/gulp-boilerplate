0c1e6dbb0adbcb38f6a2932c7264c422b686bb8a
Jomar Antonio Cardoso <jomar.cardoso@cwi.com.br>
2019-02-19 18:43:57 -0300

    feat(gulp): imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
+const gulpBabel = require('gulp-babel');
 const browsersync = require('browser-sync').create();
 const cdnizer = require("gulp-cdnizer");
 const cleanCSS = require('gulp-clean-css');
@@ -7,24 +7,27 @@ const del = require('del');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 const newer = require('gulp-newer');
 const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
+const gulpSass = require('gulp-sass');
+const gulpUglify = require('gulp-uglify');
 const webpackstream = require('webpack-stream');
 const webpackconfig = require('./webpack.config.js');
+const gulpWebp = require('gulp-webp');
 
 const NODE_ENV = process.env.NODE_ENV || 'development' // production || homologation
 
 let completePath = '';
-if (process.env.INIT_CWD) {
-  completePath =  process.env.INIT_CWD.split('/');
-} else {
+if (process.env.PWD) {
   completePath =  process.env.PWD.split('/');
+} else {
+  completePath =  process.env.INIT_CWD.split('/');
 }
 
 const FOLDER_NAME = completePath[completePath.length - 1];
 const CDN = `https://${NODE_ENV === 'homologation' ? 'test-' : ''}secure-static.arezzo.com.br/content/${process.env.CDN}/${FOLDER_NAME}/`;
+const CDN_FONTS = `https://${NODE_ENV === 'homologation' ? 'test' : 'www'}.arezzo.com.br/content/${process.env.CDN}/${FOLDER_NAME}/`;
 
 function browserSync(done) {
   browsersync.init({
@@ -57,7 +60,7 @@ function htmlCdnizer() {
     .src('./dist/**/*.html')
     .pipe(cdnizer({
       defaultCDNBase: CDN,
-      files: ['**/*.{gif,png,jpg,jpeg}']
+      files: ['**/*.{gif,png,jpg,jpeg,css,js,svg}']
     }))
     .pipe(gulp.dest('./dist/'));
 }
@@ -73,7 +76,7 @@ function css() {
   return gulp
     .src('./assets/css/**/*.scss')
     .pipe(plumber())
-    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulpSass({ outputStyle: 'expanded' }))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       grid: 'autoplace'
@@ -88,7 +91,11 @@ function cssCdnizer() {
     .pipe(cdnizer({
       defaultCDNBase: CDN,
       relativeRoot: 'css',
-      files: ['**/*.{gif,png,jpg,jpeg}']
+      files: ['**/*.{gif,png,jpg,jpeg,svg}']
+    }))
+    .pipe(cdnizer({
+      defaultCDNBase: CDN_FONTS,
+      files: ['**/*.{eot,woff,woff2,ttf}']
     }))
     .pipe(gulp.dest('./dist/css/'));
 }
@@ -100,6 +107,12 @@ function cssMinify() {
     .pipe(gulp.dest('./dist/css/'));
 }
 
+function font() {
+  return gulp
+    .src('./assets/font/**/*')
+    .pipe(gulp.dest('./dist/font/'))
+}
+
 function img() {
   return gulp
     .src('./assets/img/**/*')
@@ -109,6 +122,7 @@ function img() {
         imagemin.gifsicle({ interlaced: true }),
         imagemin.jpegtran({ progressive: true }),
         imagemin.optipng({ optimizationLevel: 5 }),
+        imageminMozjpeg({ quality: 85 }),
         imagemin.svgo({
           plugins: [
             {
@@ -122,12 +136,18 @@ function img() {
     .pipe(gulp.dest('./dist/img/'));
 }
 
+function webp() {
+  return gulp
+    .src('./assets/img/**/*')
+    .pipe(gulpWebp({ quality: 86, method: 6,  }))
+    .pipe(gulp.dest('./dist/img/'));
+}
+
 function js() {
   return gulp
     .src('./assets/js/**/*')
     .pipe(plumber())
-    .pipe(webpackstream(webpackconfig))
-    // folder only, filename is specified in webpack config
+    .pipe(webpackstream(webpackconfig))    
     .pipe(gulp.dest('./dist/js/'))
     .pipe(browsersync.stream());
 }
@@ -138,9 +158,9 @@ function jsCdnizer() {
     .pipe(cdnizer({
       defaultCDNBase: CDN,
       relativeRoot: 'js',
-      files: ['**/*.{gif,png,jpg,jpeg}']
+      files: ['**/*.{gif,png,jpg,jpeg,svg}']
     }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/js/'));
 }
 
 function watchFiles() {
@@ -150,20 +170,28 @@ function watchFiles() {
   gulp.watch('./assets/img/**/*', img);
 }
 
-function babelize() {
+function babel() {
   return gulp
   .src('./dist/js/**/*')
-  .pipe(babel({ presets: ['@babel/env'] }))
-  .pipe(uglify())
+  .pipe(gulpBabel({ presets: ['@babel/env'] }))
+  .pipe(gulpUglify())
   .pipe(gulp.dest('./dist/js/'));
 }
 
+function moveBuild() {
+  return gulp
+    .src('./dist/**/*')
+    .pipe(gulp.dest(`./dist/${NODE_ENV === 'production' ? 'prd': 'hml'}/`))
+}
+
 const watch = gulp.parallel(watchFiles, browserSync);
+const dist = gulp.parallel(css, img, js, html, font, webp);
 
 exports.build = gulp.series(
   clean,
-  gulp.parallel(css, img, js, html),
+  dist,
   gulp.parallel(cssCdnizer, htmlCdnizer, jsCdnizer),  
-  gulp.parallel(babelize, htmlMinify, cssMinify));
+  gulp.parallel(babel, htmlMinify, cssMinify),
+  moveBuild);
 
-exports.default = gulp.series(clean, gulp.parallel(css, img, js, html), watch); 
+exports.default = gulp.series(clean, dist, watch); 
